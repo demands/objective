6a6e65d6af837157990fe3b1cb0402a53fc359c0
Max Edmands <me@maxedmands.com>
2014-09-29 23:32:33 -0700

    Replace AsyncDefinitionFunction with Promise.promisify

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
+var Promise = require('bluebird')
 var ObjectFactory = require('./lib/factories/object_factory');
-var AsyncDefinitionFunction = require('./lib/async_definition_function');
 
 module.exports.def = ObjectFactory.prototype.def;
-module.exports.async = function async(fn) {
-  return new AsyncDefinitionFunction(fn);
-};
+module.exports.async = Promise.promisify;
--- test/def_and_gen.test.js
+++ test/def_and_gen.test.js
@@ -1,4 +1,3 @@
-var expect = require('chai').expect;
 var o = require('..');
 
 describe('def and gen', function() {
