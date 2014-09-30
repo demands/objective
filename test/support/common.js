34e2d84ec5ba89e9c3d42d7e9fe80c18aa599578
Max Edmands <me@maxedmands.com>
2014-09-29 23:42:03 -0700

    Given function in tests

--- test/support/common.js
+++ test/support/common.js
@@ -3,3 +3,6 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 GLOBAL.expect = chai.expect;
+GLOBAL.given = function given(description, func) {
+  describe('given ' + description, func);
+};
